d49dba17c79106dd712de0629f41f618c5812341
Iran Alyf <iranalyf2222@gmail.com>
2023-01-07 17:20:57 -0300

    innitiitial commit

---
+++ development/nautobot_config.py
@@ -0,0 +1,38 @@
+"""Nautobot development configuration file."""
+import os
+
+from nautobot.core.settings import *  # noqa: F403
+from nautobot.core.settings_funcs import is_truthy
+
+#
+# Debugging defaults to True rather than False for the development environment
+#
+DEBUG = is_truthy(os.getenv("NAUTOBOT_DEBUG", "True"))
+
+# Django Debug Toolbar - enabled only when debugging
+if DEBUG:
+    if "debug_toolbar" not in INSTALLED_APPS:  # noqa: F405
+        INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
+    if "debug_toolbar.middleware.DebugToolbarMiddleware" not in MIDDLEWARE:  # noqa: F405
+        MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+    # By default the toolbar only displays when the request is coming from one of INTERNAL_IPS.
+    # For the Docker dev environment, we don't know in advance what that IP may be, so override to skip that check
+    DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda _request: DEBUG}
+
+#
+# Logging for the development environment, taking into account the redefinition of DEBUG above
+#
+
+LOG_LEVEL = "DEBUG" if DEBUG else "INFO"
+LOGGING["loggers"]["nautobot"]["handlers"] = ["verbose_console" if DEBUG else "normal_console"]  # noqa: F405
+LOGGING["loggers"]["nautobot"]["level"] = LOG_LEVEL  # noqa: F405
+
+#
+# Plugins
+#
+
+PLUGINS = [
+    "example_plugin",
+]
+
+METRICS_ENABLED = True
